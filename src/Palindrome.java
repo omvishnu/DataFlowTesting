0227f49cdce6f8b8b3a08d4a22d1182a0ecb0f45
Hong Joon Chew <hongjoon.chew@gmail.com>
2017-10-05 19:23:10 -0700

    Cleanup

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,7 +1,8 @@
 public class Palindrome {
 	
 	public boolean isPalindrome(String s) {
-		int tail = s.length()-1;		
+		int tail = s.length()-1;
+		System.out.println(s.length());
 		for (int head = 0; head < (s.length()-1)/2; head++) {
 			char front = s.charAt(head);
 			char back = s.charAt(tail--);
