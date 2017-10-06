75ef19063e0b559de8d0d48fcf0965f172e7dccc
Hong Joon Chew <hongjoon.chew@gmail.com>
2017-10-05 19:29:43 -0700

    convert string to lowercase first

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -2,9 +2,10 @@ public class Palindrome {
 	
 	public boolean isPalindrome(String s) {
 		int tail = s.length()-1;
+		s = s.toLowerCase();
 		System.out.println(s.length());
 		for (int head = 0; head < (s.length()-1)/2; head++) {
-			char front = s.charAt(head);
+			char front = (s.charAt(head));
 			char back = s.charAt(tail--);
 			if(front != back) {
 				return false;
